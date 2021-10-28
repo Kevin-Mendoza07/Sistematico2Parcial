624aad92fd4039c145368a3b7158bd2815688bbd
KevinMen7 <kevingfx71@gmail.com>
2021-10-28 10:52:31 -0600

    Subiendo

---
+++ Infraestructura/ActivosFijos/ActivoFijoModel.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructura.ActivosFijos
+{
+    public class ActivoFijoModel
+    {
+        private ActivoFijo[] activoFijos;
+    }
+}
